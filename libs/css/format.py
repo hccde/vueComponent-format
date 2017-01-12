04bbabfda08e2285b5de8f79994011a5651b56a0
hccde <1076663958@qq.com>
2017-01-12 22:31:13 +0800

    parse css

--- libs/css/format.py
+++ libs/css/format.py
@@ -5,12 +5,31 @@ class CssFormat:
 		print css_str
 
 class Paser(object):
+	#state 0 '}'
+	#state 1 string
+	#state 2 '{'
+	#state 3 ';'
 	strings=''
 	def __init__(self, css_str):
 		self.strings = css_str.strip()
 	def get_css_obj(self):
+		length = len(self.strings)
+		string = self.string
+		brace_stack=[];
+		
+		state = {
+			collector:'',
+			state:'1'
+		}
+		obj = {
+			selector:'',
 
-		pass;
+			# child
+		}
+		for index in range(0,length):
+			if string[index]=='{' and state['state']==1:
+				pass
+				
 		
 		
 
