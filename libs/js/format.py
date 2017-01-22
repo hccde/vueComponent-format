7512d7014d0854e5963e0fc5846c3f8d9766c713
hccde <1076663958@qq.com>
2017-01-22 18:07:42 +0800

    wrapping class

--- libs/js/format.py
+++ libs/js/format.py
@@ -1,10 +1,21 @@
 import jsbeautifier
-options = jsbeautifier.default_options()
-options.indent_size = 4
-options.indent_char = ' '
-options.preserve_newlines = True
-options.jslint_happy = False
-options.keep_array_indentation = False
-options.brace_style = 'collapse'
-options.indent_level = 0
-print jsbeautifier.beautify('    var foo = function(){ bar() }();   ', options)
+
+#format js string 
+class CssFormat:
+    options={}
+    js_str = ''
+    def __init__(self, js_str,settings):
+        js_str = '    var foo = function(){ bar() }();'
+        self.js_str = js_str
+        options = jsbeautifier.default_options()
+        options.indent_size = 4
+        options.indent_char = ' '
+        options.preserve_newlines = True
+        options.jslint_happy = False
+        options.keep_array_indentation = False
+        options.brace_style = 'collapse'
+        options.indent_level = 0
+        self.options = options
+        self.format()
+    def format(self):
+        return jsbeautifier.beautify(self.js_str, self.options)
